1a540f91cc9aea440ae505aa567bdbf136ee4b9c
XzynAstralz <xzynggx@gmail.com>
2024-06-08 12:49:35 -0500

    Update list.lua

--- list.lua
+++ list.lua
@@ -6,5 +6,13 @@ return {
                 color = Color3.fromRGB(255, 255, 255)
             }
         }
+    },    
+    ["877194d06a3bb4fd09922c79a408b98367e394a5243d781388a210d00853b197f08f9ab95ec9f6f8ee56bf03d043c8f969ba219a841fe588ec22e5154997f939"] = {
+        tags = {
+            {
+                text = "Niga",
+                color = Color3.fromRGB(0, 0, 0)
+            }
+        }
     },
 }
