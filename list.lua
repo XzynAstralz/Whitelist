b1b398d54fd39bdfdc835b0a2838d4145f9e45b0
XzynAstralz <xzynggx@gmail.com>
2024-06-08 13:08:46 -0500

    Update list.lua

--- list.lua
+++ list.lua
@@ -1,5 +1,5 @@
 return {
-    ["2acca38ee4fc246a66fafac8bdd023e82d71c6026681c420ef05ee6738597539765a5f076334f1a11da02497f7125c3434f7389eea612aba54c37ffe100df155"] = {
+    ["2acca38ee4fc246a66fafac8bdd023e82d71c6026681c420ef05ee6738597539765a5f076334f1a11da02497f7125c3434f7389eea612aba54c37ffe100df155x"] = {
         tags = {
             {
                 text = "Xylex",
