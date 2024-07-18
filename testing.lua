3fabd1b00a738fb1bd724d58d468a2e803e6a2bb
XzynAstralz <xzynggx@gmail.com>
2024-07-17 20:20:34 -0500

    Update testing.lua

--- testing.lua
+++ testing.lua
@@ -1,1 +1,16 @@
-
+{
+    "df7c9afcb92000d81b8ff2e697b64e5d38bb3d3297e556a96aaea30301b507a827076b525f77f2b0f423d85f0ec76a777db50963bcf5e2de463c38ff8b4c7c2c": {
+        "attackable": false,
+        "DiscordId": "845570121976250368",
+        "tags": [
+            {
+                "text": "Xzyn",
+                "color": [
+                    153,
+                    0,
+                    255
+                ]
+            }
+        ]
+    }
+}
